2e887f80afef2f517e4a2bf85369c5255606fc2f
Ruben Bermudez Saez <rubenbermudezsaez@gmail.com>
2022-09-26 23:21:15 +0200

    Main Header improved

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,11 @@ module.exports = {
 	darkMode: 'class',
 	content: ['./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			animation: {
+				'pulse-slow': 'pulse 1s linear infinite',
+			},
+		},
 	},
 	plugins: [],
 };
